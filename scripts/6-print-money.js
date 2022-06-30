a85b999d9d178c477f25ba53dfe62f576c5e16c9
Asia K <asialakaygrady@gmail.com>
2022-06-29 22:10:23 -0400

    Created the token’s supply

---
+++ scripts/6-print-money.js
@@ -0,0 +1,18 @@
+import sdk from "./1-initialize-sdk.js";
+
+const token = sdk.getToken("0xeEe746dcE397378567039d845740D9bf28Fb399D");
+
+(async () => {
+    try {
+        // max token supply
+        const amount = 1_000_000;
+        // interact with the deployed ERC-20 contract and mint the tokens
+        await token.mintToSelf(amount);
+        const totalSupply = await token.totalSupply();
+
+        // printing to the console how many tokens now exist
+        console.log("💸 There are now ", totalSupply.displayValue, "$UPCY in circulation");  
+    } catch (error){
+        console.error("😿 Failed to print money", error);  
+    }
+})();
